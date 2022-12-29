f44108a0e6ce6310c2c7c39d3c10c6dbe328270c
unknown <ernesto.rapisarda81@gmail.com>
2022-12-29 11:49:26 +0100

    creati frontend e backend

---
+++ BackEnd/src/main/java/it/sad/students/eventboard/EventBoardApplication.java
@@ -0,0 +1,13 @@
+package it.sad.students.eventboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventBoardApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/it/sad/students/eventboard/EventBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sad.students.eventboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
