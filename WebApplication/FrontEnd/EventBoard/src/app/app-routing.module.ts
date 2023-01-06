ddae6a9ec8318cf68241ddabc02e765cfb61a184
Alessandro <alessandromonetti@outlook.it>
2023-01-06 10:49:47 +0100

    Aggiunti componenti, aggiunti services

--- WebApplication/FrontEnd/EventBoard/src/app/app-routing.module.ts
+++ WebApplication/FrontEnd/EventBoard/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "./components/homepage/homepage.component";
+import {CreateEventComponent} from "./components/create-event/create-event.component";
+import {EventComponent} from "./components/event/event.component";
+import {LoginComponent} from "./components/login/login.component";
+import {SearchComponent} from "./components/search/search.component";
+import {SignUpComponent} from "./components/sign-up/sign-up.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'create-event', component: CreateEventComponent},
+  {path: 'event', component: EventComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WebApplication/FrontEnd/EventBoard/src/app/app.module.ts
+++ WebApplication/FrontEnd/EventBoard/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { CardComponent } from './components/card/card.component';
+import { CreateEventComponent } from './components/create-event/create-event.component';
+import { EventComponent } from './components/event/event.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SearchComponent } from './components/search/search.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CardComponent,
+    CreateEventComponent,
+    EventComponent,
+    HomepageComponent,
+    LoginComponent,
+    NotFoundComponent,
+    SearchComponent,
+    SignUpComponent,
+    TopBarComponent,
   ],
   imports: [
     BrowserModule,
