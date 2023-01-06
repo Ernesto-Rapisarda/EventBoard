21689bd8c904c0d5d4f05424d3b8d72f5a8bd768
AndreaYpmY <andrea.tocci151001@gmail.com>
2023-01-06 09:47:06 +0100

    DemoData aggiunto like

--- WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/DemoData.java
+++ WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/DemoData.java
@@ -13,17 +13,25 @@ public class DemoData {
 
     public DemoData(){
 
-        /*Position position=createPosition();
+        /*
+                    POSITION
+        Position position=createPosition();
         DBManager.getInstance().getPositionDao().saveOrUpdate(position);
+        */
 
+        /*
+                    EVENT_TYPE
         EventType eventType2= createEventType("porno","easy peasy");
         DBManager.getInstance().getEventTypeDao().saveOrUpdate(eventType2);
         EventType eventType=createEventType("azione","Nu burdell");
         DBManager.getInstance().getEventTypeDao().saveOrUpdate(eventType);
+        */
+
 
         //System.out.println(DBManager.getInstance().getPositionDao().findAll().get(0).getId());
 
-
+        /*
+                    PERSON
         Person person1=createPerson("Andrea","Tocci","YocoPocoMaYoco",
                 "andrea@tcc",true, 1L, Role.ADMIN,"password");
         DBManager.getInstance().getPersonDao().saveOrUpdate(person1);
@@ -32,37 +40,52 @@ public class DemoData {
                 "simo@tcc",true, 2L, Role.USER,"password");
         DBManager.getInstance().getPersonDao().saveOrUpdate(person2);
 
-
         Person person3=createPerson("Alessandro","Monetti","Pingu",
                 "pingu@tcc",true, 1L, Role.PUBLISHER,"password");
         DBManager.getInstance().getPersonDao().saveOrUpdate(person3);
 
-
         Person person4=createPerson("Ernesto","Rapisarda","Nocera",
                 "erne@tcc",true, 2L, Role.PUBLISHER,"password");
         DBManager.getInstance().getPersonDao().saveOrUpdate(person4);
-*/
-
-        LocalDateTime date = LocalDateTime.now();
-        date = date.minusSeconds(ThreadLocalRandom.current().nextInt(1, 30326400));
-
-
-        Event event1 =createEvent(1L, LocalDate.from(date), 2L,10.5,
-                "nulla piu totale",false, "Evento incredibile",
-                DBManager.getInstance().getPersonDao().findByUsername("Pingu").getId());
-        DBManager.getInstance().getEventDao().saveOrUpdate(event1);
-
-        Event event2 =createEvent(2L,LocalDate.from(date), 1L,10.5,
-                "easy easy",false, "Evento incredibile",
-                DBManager.getInstance().getPersonDao().findByUsername("Nocera").getId());
-        DBManager.getInstance().getEventDao().saveOrUpdate(event2);
+        */
 
 
 
         /*
+                    EVENT
+        Event event1 =createEvent(1L, LocalDate.from(dateTime()), 2L,10.5,
+                "nulla piu totale",false, "Evento incredibile",
+                DBManager.getInstance().getPersonDao().findByUsername("Pingu").getId());
+        DBManager.getInstance().getEventDao().saveOrUpdate(event1);
 
-         */
+        Event event2 =createEvent(2L,LocalDate.from(dateTime()), 1L,10.5,
+                "easy easy",false, "Evento incredibile",
+                DBManager.getInstance().getPersonDao().findByUsername("Nocera").getId());
+        DBManager.getInstance().getEventDao().saveOrUpdate(event2);
+        */
 
+
+        /*
+                    LIKE
+        Like like1=createLike(24L,4L,LocalDate.from(dateTime()));
+        DBManager.getInstance().getLikeDao().saveOrUpdate(like1);
+        Like like2=createLike(24L,3L,LocalDate.from(dateTime()));
+        DBManager.getInstance().getLikeDao().saveOrUpdate(like2);
+        Like like3=createLike(18L,4L,LocalDate.from(dateTime()));
+        DBManager.getInstance().getLikeDao().saveOrUpdate(like3);
+        Like like4=createLike(17L,3L,LocalDate.from(dateTime()));
+        DBManager.getInstance().getLikeDao().saveOrUpdate(like4);
+        */
+
+
+
+
+    }
+
+    public LocalDateTime dateTime(){ //data casuale
+        LocalDateTime date = LocalDateTime.now();
+        date = date.minusSeconds(ThreadLocalRandom.current().nextInt(1, 30326400));
+        return date;
     }
 
     public Position createPosition(){
