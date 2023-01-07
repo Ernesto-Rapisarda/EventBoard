e21ca0444bce39a287bbc5434db7ba53c0d84b8d
Ernesto <ernesto.rapisarda81@gmail.com>
2023-01-07 21:23:23 +0100

    fixato problema di avvio di una service

--- WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/service/StatusCodes.java
+++ WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/service/StatusCodes.java
@@ -2,10 +2,14 @@ package it.sad.students.eventboard.service;
 
 
 import it.sad.students.eventboard.persistenza.DBManager;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
-
+@Service
+@AllArgsConstructor
 public class StatusCodes {
 
     public ResponseEntity unauthorized(){return ResponseEntity.badRequest().body("Unauthorized access to the resource");}
