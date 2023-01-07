93bc1a596c2070ffdc1fee91185e9da4159d9f33
Ernesto <ernesto.rapisarda81@gmail.com>
2023-01-07 17:56:35 +0100

    completata api delete event con opportuni codice di errore

--- WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/controller/api/EventController.java
+++ WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/controller/api/EventController.java
@@ -7,18 +7,22 @@ import it.sad.students.eventboard.persistenza.model.Person;
 import it.sad.students.eventboard.security.auth.AuthenticationService;
 import it.sad.students.eventboard.security.config.JwtService;
 import it.sad.students.eventboard.service.EventService;
+import it.sad.students.eventboard.service.ResponseEventCreation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.imageio.ImageIO;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @RestController
-//@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:4200")
 @RequiredArgsConstructor
 public class EventController {
 
@@ -27,37 +31,29 @@ public class EventController {
     private final JwtService jwtService;
 
     @RequestMapping(value="/api/create/event",method= RequestMethod.POST)
-    public ResponseEntity<Long> createEvent(@RequestBody Event event ){
+    public ResponseEntity<ResponseEventCreation> createEvent(@RequestBody Event event, @RequestHeader (name="Authorization") String token){
+        //utente non autorizzato 403
+        //badrequest errore nel db nella creazione 400
+        //evento nullo 404 not found
+        //inserimento effettuato codice 200 ok
+        return eventService.createEvent(event,token);
 
-        event.setDate(LocalDate.from(LocalDateTime.now()));
-
-        Boolean status = eventService.createEvent(event);
-        if(status)
-            return ResponseEntity.ok(event.getId());
-        else
-            return ResponseEntity.ok(null);
     }
+
+
 
     @RequestMapping(value="/api/delete/event/{id}",method= RequestMethod.DELETE)
     public ResponseEntity deleteEvent(@PathVariable Long id,@RequestHeader (name="Authorization") String token ){
-
+        //utente non autorizzato 403
+        //organizer non proprietario dell'evento 400
+        //evento non trovato 404 not found
+        //rimozione effettuata codice 200 ok
         return eventService.deleteEvent(id,token);
     }
 
-/*
-    @PreAuthorize("hasRole('PUBLISHER')")
-    @RequestMapping(value="/api/create/event",method= RequestMethod.POST)
-    public ResponseEntity<Long> createEvent(@RequestBody Event event , String token, String username){
 
-        if (jwtService.isTokenValid(token, DBManager.getInstance().getPersonDao().findByUsername(username)))
-            event.setDate(LocalDate.from(LocalDateTime.now()));
 
-        Boolean status = eventService.createEvent(event);
-        if(status)
-            return ResponseEntity.ok(event.getId());
-        else
-            return ResponseEntity.ok(null);
-    }*/
+
 
     //update
     //delete
---
+++ WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/service/ResponseEventCreation.java
@@ -0,0 +1,14 @@
+package it.sad.students.eventboard.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseEventCreation {
+    private Long id;
+}
