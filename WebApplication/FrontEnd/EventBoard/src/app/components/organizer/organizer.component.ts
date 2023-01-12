20eafa10ac7d0ea84d06a43e91ee1f7f43ac0c79
Simone Rotundo <simone1377.rotundo@gmail.com>
2023-01-12 16:21:45 +0100

    FE: Il component per le review è stato personalizzato

---
+++ WebApplication/FrontEnd/EventBoard/src/app/components/organizer/organizer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer',
+  templateUrl: './organizer.component.html',
+  styleUrls: ['./organizer.component.css']
+})
+export class OrganizerComponent {
+
+}
