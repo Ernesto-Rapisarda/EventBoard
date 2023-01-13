38eb0f61b73dd8194b1a498ff7946c59c6c23c48
Simone Rotundo <simone1377.rotundo@gmail.com>
2023-01-13 23:21:39 +0100

    FE: Il pulsante per selezionare la valutazione appropriata, ora è dinamica

--- WebApplication/FrontEnd/EventBoard/src/app/components/review-form/review-form.component.ts
+++ WebApplication/FrontEnd/EventBoard/src/app/components/review-form/review-form.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ReviewFormComponent {
 
+  private min = 1;
+  private max = 10;
+
+  review: number = 6;
+
+  private updateReview() {
+    this.review++;
+    if (this.review > this.max) {
+      this.review = this.min;
+    }
+  }
+
+
 }
