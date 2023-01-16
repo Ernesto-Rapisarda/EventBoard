c1ba1e8683093f4795ac4ebbc8cf4d3b9c625f7a
Simone Rotundo <simone1377.rotundo@gmail.com>
2023-01-16 10:23:32 +0100

    FE: modificate icone nel create-event.component.html, event-edit-dialog.component.html abbozzato

--- WebApplication/FrontEnd/EventBoard/src/app/components/event-edit-dialog/event-edit-dialog.component.ts
+++ WebApplication/FrontEnd/EventBoard/src/app/components/event-edit-dialog/event-edit-dialog.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {RequestService} from "../../services/request.service";
+import {AuthService} from "../../auth/auth.service";
+import {Router} from "@angular/router";
+import {ImgApiService} from "../../services/img-api.service";
 
 @Component({
   selector: 'app-event-edit-dialog',
@@ -6,5 +11,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./event-edit-dialog.component.css']
 })
 export class EventEditDialogComponent {
+  eventEditDialog: FormGroup;
+  urlPoster: string;
+  private eventCreateForm: FormGroup<{ date: FormControl<string>; ticketUrl: FormControl<string>; price: FormControl<string>; description: FormControl<string>; location: FormControl<string>; eventType: FormControl<string>; title: FormControl<string> }>;
+
+  constructor(private requestService: RequestService, private authService: AuthService, private router: Router, private imgService: ImgApiService) { }
+
+  eventTypes: string[]
+
+  ngOnInit(): void {
+    this.urlPoster = "" // TODO: vanno assegnati quelli già impostati
+
+    this.eventCreateForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      date: new FormControl(''),
+      location: new FormControl('', [Validators.required]),
+      price: new FormControl('', [Validators.required, Validators.pattern('^((\\d+)|(\\d+\\.\\d+))$')]),
+      eventType: new FormControl('', Validators.required),
+      ticketUrl: new FormControl(''),
+      description: new FormControl('')
+    })
+
+  }
 
 }
