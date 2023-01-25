beb9db02c23f460120e5e956f218a1459e2d6357
Alessandro <alessandromonetti@outlook.it>
2023-01-26 00:29:58 +0100

    BE: Aggiunte alcune tipologie di evento

--- WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/persistenza/model/EventType.java
+++ WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/persistenza/model/EventType.java
@@ -1,5 +1,5 @@
 package it.sad.students.eventboard.persistenza.model;
 
 public enum EventType {
-    culturale,teatrale,sportivo,piazza,concerto,sagre
+    culturale,teatrale,sportivo,piazza,concerto,sagre,religione,tecnologia,popolare,fiera,motori,politica,beneficienza
 }
