9987e500c1d21c2f037c30d922ca462b4077493f
AndreaYpmY <andrea.tocci151001@gmail.com>
2023-01-26 15:48:17 +0100

    Fix message report pt.2

--- WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/service/ReportService.java
+++ WebApplication/BackEnd/src/main/java/it/sad/students/eventboard/service/ReportService.java
@@ -39,7 +39,7 @@ public class ReportService {
 
             String message=report.getMessage();
             if(type.equals("review"))
-                message=message+"\n\n| Oggetto segnalazione: "+type+"  id evento: "+id+"  -id persona segnalata: "+person+" |";
+                message=message+"\n\n| Oggetto segnalazione: "+type+"  id evento: "+id+"   id persona segnalata: "+person+" |";
             else
                 message=message+"\n\n| Oggetto segnalazione: "+type+"  id oggetto segnalato: "+id+" |";
             report.setMessage(message);
