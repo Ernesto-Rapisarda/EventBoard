982f12d95a47908ecc6a699140f5f07caadb8b4b
Alessandro <alessandromonetti@outlook.it>
2023-01-30 16:11:11 +0100

    BE: Aggiunto pulsante scroll-to-top nella pagina thymeleaf

--- WebApplication/BackEnd/src/main/resources/static/js/stats.js
+++ WebApplication/BackEnd/src/main/resources/static/js/stats.js
@@ -1,7 +1,28 @@
-function redirect(id){
+let scrollBtn;
+
+window.onload = function() {
+  scrollBtn = document.querySelector("#scrollBtn");
+
+  window.onscroll = function() {scrollCheck()};
+}
+function scrollCheck() {
+  if (window.scrollY > 400) {
+    scrollBtn.style.display = "flex";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+function stats(id){
   location.href = `http://localhost:4200/event/${id}`;
 }
 
+// TODO: Rimuovere il codice sottostante se non necessario
 function checkButton(){
   const user=localStorage.getItem("token")
 
